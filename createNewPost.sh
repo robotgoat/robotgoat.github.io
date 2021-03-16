923987a7806e9368b97c407aada9df4e5a2f8ffe
robotgoat <robotgoat@users.noreply.github.com>
2021-03-15 21:31:02 -0400

    E46 coupe post added

---
+++ createNewPost.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Create new Hugo post
+
+Help()
+{
+	# Help message
+	echo "Script to create a new post in this Hugo website"
+	echo 
+	echo "Syntax: ./createNewPost Title-Of-Post"
+	echo "Ensure spaces in title are replaced with dashes"
+	echo
+}
+
+
+while getopts ":h" option; do
+	case $option in
+		h)
+			Help
+			exit;;
+	esac
+done
+
+
+hugo new --kind post-bundle posts/${today}_${1}
